37002c8db9a4fba80108ff26194fb5911c23b22f
Shiva936 <shivadharshansankar936@gmail.com>
2023-10-05 22:22:16 +0530

    Added comments

--- Main.java
+++ Main.java
@@ -15,7 +15,8 @@ public class Main{
         customers.add(new Customer("13456", "75689", 28697,"bb"));
         customers.add(new Customer("21456", "40689", 1286970,"cc"));
         customers.add(new Customer("23156", "45189", 1836907,"dd"));
-        
+        // Use Ctrl-c to exit!!! As ATMs don't have an inbuilt quit option
+        System.out.println("Use CTRL+C to close the ATM\nATM does not have option to quit\nRequired because of nature of ATMs\n");
         new ATM(sc,customers,new int[] {1,2,3,4,1,7,3,1});// An instance of ATM is created to use the atm
 
     }
